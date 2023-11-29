46608cc1fd8622a89dfdb626619112d9c3aa26ba
aaron <aaron.lhh0401@gmail.com>
2023-11-29 05:15:52 -0800

    toggle switch styling and functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,70 +1,29 @@
-const form = document.getElementById("contact-form")
-const email = document.getElementById("email");
-const name = document.getElementById("name");
-const comments = document.getElementById("comments");
-const error_message = document.getElementById("error")
-const info_message = document.getElementById("info")
+const toggle_switch = document.querySelector("label.switch");
+const switch_button = document.querySelector("label.switch > input");
+const root_theme = document.querySelector(':root');
+toggle_switch.classList.remove("nojs");
 
-// validity checks
-document.addEventListener("DOMContentLoaded", function() {
-    if (email.validity.typeMismatch || email.validity.valueMissing) {
-        email.setCustomValidity("An email address is expected!");
-    } else {
-        email.setCustomValidity("");
-    }
+if(localStorage.getItem("theme") == "light"){
+    localStorage.setItem("theme", "light");
+    root_theme.style.setProperty("--background-color", "white");
+    switch_button.checked = true;
+}else if(localStorage.getItem("theme") == "dark"){
+    localStorage.setItem("theme", "dark");
+    root_theme.style.setProperty("--background-color", "grey");
+    switch_button.checked = false;
+}
 
-    if (name.validity.valueMissing) {
-        name.setCustomValidity("The name field cannot be empty!");
-    } else {
-        name.setCustomValidity("");
-    }
-
-    if (comments.validity.valueMissing) {
-        comments.setCustomValidity("The message field cannot be empty!");
-    } else {
-        comments.setCustomValidity("");
-    }
-})
-
-email.addEventListener("input", (event) => {
-    if (email.validity.typeMismatch || email.validity.valueMissing) {
-        email.setCustomValidity("An email address is expected!");
-    } else {
-        email.setCustomValidity("");
-    }
-});
-
-name.addEventListener("input", (event) => {
-    if (name.validity.valueMissing) {
-        name.setCustomValidity("The name field cannot be empty!");
-    } else {
-        name.setCustomValidity("");
-    }
-});
-
-comments.addEventListener("input", (event) => {
-    if (comments.validity.valueMissing) {
-        comments.setCustomValidity("The message field cannot be empty!");
-    } else {
-        comments.setCustomValidity("");
-    }
-
-    // pattern="[a-zA-Z0-9]*"
-    if(/[@#$%^&*()_+\-=\[\]{}\\|<>\/]/.test(comments.value)){
-        comments.classList.add("invalid");
-        error_message.value = "Please only include numbers, letters and punctuations";
-        error_message.classList.add("invalid");
+// toggle switch
+switch_button.addEventListener("click", ()=>{
+    if(!switch_button.checked){
+        localStorage.setItem("theme", "dark");
+        root_theme.style.setProperty("--background-color", "grey");
     }else{
-        comments.classList.remove("invalid")
-        error_message.value = ""
-        error_message.classList.remove("invalid");
-    }
-
-    if(comments.maxLength - comments.value.length <= 10){
-        info_message.value = `Current number of characters: ${comments.value.length} out of maxLength: ${comments.maxLength}`;
-    }else{
-        info_message.value = ""
+        localStorage.setItem("theme", "light");
+        root_theme.style.setProperty("--background-color", "white");
     }
 });
+
+
 
 
